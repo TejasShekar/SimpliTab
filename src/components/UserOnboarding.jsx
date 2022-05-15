8d57f9afc9b3586b18cfe2d49ce8ddd572091cbe
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-05-16 01:40:36 +0530

    feat: user onboarding

---
+++ src/components/UserOnboarding.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const UserOnBoarding = () => {
+  const [error, setError] = useState(false);
+  const userHandler = (e) => {
+    if (e.target.value === "") {
+      setError(true);
+    }
+    if (e.key === "Enter" && e.target.value !== "") {
+      localStorage.setItem("p8extension", e.target.value);
+      e.target.value = "";
+    }
+  };
+
+  return (
+    <>
+      <h2 className="p-2">Hello, what's your name?</h2>
+      <input
+        className="userInput text-center"
+        type="text"
+        onKeyPress={userHandler}
+        onChange={() => setError(false)}
+      />
+      {error && (
+        <p className="pt-2 error text-center">Please enter a valid input</p>
+      )}
+    </>
+  );
+};
