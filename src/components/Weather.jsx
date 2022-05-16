755723b59067bb32a2579d170d6f161123c62166
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-05-16 19:52:15 +0530

    feat: weather widget
    - Added weather widget
    - Make site responsive

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const Weather = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (x) => {
+      try {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${x.coords.latitude}&lon=${x.coords.longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+        );
+        setData(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, []);
+
+  //render nothing if there is no data
+  if (!data) {
+    return;
+  }
+
   return (
-    <>
-      <h2>Weather</h2>
-    </>
+    <div
+      className="flex-center"
+      style={{ position: "absolute", top: "0", right: "0", padding: "1rem" }}
+    >
+      <img
+        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        alt="weather"
+      />
+      <div>
+        <h3>{Math.floor(data.main.temp)} &deg;C</h3>
+        <p>{data.name}</p>
+      </div>
+    </div>
   );
 };
