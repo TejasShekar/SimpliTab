a639ff5c96396b756c555c9dea81835c83afa2ed
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-05-19 21:53:43 +0530

    fix: made checkbox state persistant
    was getting the checked state from local storage which was a string instead of boolean.

--- src/components/MainFocus.jsx
+++ src/components/MainFocus.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export const MainFocus = () => {
   const [focus, setFocus] = useState(null);
-  const [checked, setChecked] = useState(false);
+  const [isChecked, setChecked] = useState(false);
   const [editMode, setEditMode] = useState(true);
 
   const handleFocusInput = (e) => {
@@ -20,56 +20,23 @@ export const MainFocus = () => {
 
   const deleteFocus = () => {
     localStorage.removeItem("p8focus");
+    localStorage.removeItem("isChecked");
     setEditMode(true);
     setFocus(null);
     setChecked(false);
   };
 
   useEffect(() => {
+    setChecked(JSON.parse(localStorage.getItem("isChecked")) || isChecked);
     if (localStorage.getItem("p8focus") !== null) {
       setFocus(localStorage.getItem("p8focus"));
       setEditMode(false);
     }
-  }, []);
+  }, [isChecked]);
 
   return (
     <>
-      {!editMode ? (
-        <>
-          <div className="flex-center focus mx-2">
-            <input
-              type="checkbox"
-              id="main-focus"
-              onClick={() => setChecked((prev) => !prev)}
-            />
-            <label htmlFor="main-focus">
-              <p
-                className="focus-text pl-1 text-center"
-                style={
-                  checked
-                    ? { textDecoration: "line-through" }
-                    : { textDecoration: "none" }
-                }
-              >
-                My main focus is to {focus}
-              </p>
-            </label>
-            {!checked && (
-              <button className="focus-edit" onClick={editFocus}>
-                <i className="fas fa-pen"></i>
-              </button>
-            )}
-            <button className="focus-delete" onClick={deleteFocus}>
-              <i className="fas fa-trash-alt"></i>
-            </button>
-          </div>
-          {checked ? (
-            <p className="pt-2">Great Job !</p>
-          ) : (
-            <p className="pt-2">You can do this</p>
-          )}
-        </>
-      ) : (
+      {editMode ? (
         <>
           <p className="focus-text pt-2 text-center">
             What is your main focus today?
@@ -80,6 +47,45 @@ export const MainFocus = () => {
             className="userInput text-center"
             onKeyDown={handleFocusInput}
           />
+        </>
+      ) : (
+        <>
+          <div className="flex-center focus mx-2">
+            <input
+              type="checkbox"
+              id="main-focus"
+              onChange={() => {
+                setChecked((prev) => !prev);
+                localStorage.setItem("isChecked", !isChecked);
+              }}
+              checked={Boolean(isChecked)}
+            />
+            <label htmlFor="main-focus">
+              <p
+                className="focus-text pl-1 text-center"
+                style={
+                  isChecked
+                    ? { textDecoration: "line-through" }
+                    : { textDecoration: "none" }
+                }
+              >
+                My main focus is to {focus}
+              </p>
+            </label>
+            {!isChecked && (
+              <button className="focus-edit" onClick={editFocus}>
+                <i className="fas fa-pen"></i>
+              </button>
+            )}
+            <button className="focus-delete" onClick={deleteFocus}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
+          </div>
+          {isChecked ? (
+            <p className="pt-2">Great Job !</p>
+          ) : (
+            <p className="pt-2">You can do this</p>
+          )}
         </>
       )}
     </>
